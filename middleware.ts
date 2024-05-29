30eac8e8c1887052fa64a01daceaadc6523e6666
jovit-mathew236 <jovitmathew236@gmail.com>
2024-05-29 16:53:44 +0530

    [FIX] accessToken variable name changed

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const accessToken = request.cookies.get("accessToken");
+  const accessToken = request.cookies.get("access_token");
 
   if (request.nextUrl.pathname.startsWith("/signin") && accessToken) {
     return NextResponse.redirect(new URL("/dashboard/profile", request.url));

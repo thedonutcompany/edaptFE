285173c4a83ddc7c213884e27ce2dc7828d58ab0
jovit-mathew236 <jovitmathew236@gmail.com>
2024-05-29 16:52:05 +0530

    [FEAT] added new middleware to check the user is loged in or not to give access to dashboard route

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,15 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token");
+  const accessToken = request.cookies.get("accessToken");
 
-  if (request.nextUrl.pathname.startsWith("/signin") && token) {
+  if (request.nextUrl.pathname.startsWith("/signin") && accessToken) {
     return NextResponse.redirect(new URL("/dashboard/profile", request.url));
+  } else if (
+    request.nextUrl.pathname.startsWith("/dashboard") &&
+    !accessToken
+  ) {
+    return NextResponse.redirect(new URL("/signin", request.url));
   }
 
   return NextResponse.next();
